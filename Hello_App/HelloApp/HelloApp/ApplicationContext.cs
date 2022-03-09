658dff339872af950698e9b41db0da47dc6e8770
Llsskk <92019037+Llsskk@users.noreply.github.com>
2022-03-09 23:59:56 +0300

    Add files via upload

---
+++ Hello_App/HelloApp/HelloApp/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using HelloApp;
+using Microsoft.EntityFrameworkCore;
+namespace DemoAppCRUD
+{
+    class ApplicationContext : DbContext
+    {
+        public DbSet<User>Users { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+        : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
